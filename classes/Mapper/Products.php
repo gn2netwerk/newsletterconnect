8c9e019707d9a1f93920cef562a5bc4b87698bf6
Dave <dh@gn2-netwerk.de>
2013-10-24 15:16:52 +0200

    verlorene Änderungen von 22.08

--- classes/Mapper/Products.php
+++ classes/Mapper/Products.php
@@ -154,7 +154,9 @@ class GN2_NewsletterConnect_Mapper_Products
                 for ($i=0;$i<12;$i++) {
                     $lastPicture = $picture;
                     $picture = $article->getPictureUrl($i+1);
-                    if (strpos($picture, 'nopic.jpg') === false && $lastPicture != $picture) {
+                    if (strpos($picture, 'nopic.jpg') === false
+                        && $lastPicture != $picture
+                    ) {
                         $product->pictures[] = $picture;
                     }
                 }
@@ -166,7 +168,9 @@ class GN2_NewsletterConnect_Mapper_Products
                 for ($i=0;$i<12;$i++) {
                     $lastPicture = $picture;
                     $picture = $article->getThumbnailUrl($i+1);
-                    if (strpos($picture, 'nopic.jpg') === false && $lastPicture != $picture) {
+                    if (strpos($picture, 'nopic.jpg') === false
+                        && $lastPicture != $picture
+                    ) {
                         $product->thumbnails[] = $picture;
                     }
                 }
@@ -184,15 +188,120 @@ class GN2_NewsletterConnect_Mapper_Products
                         = $attributeEntry;
                 }
 
+                /* new return values (August 2013) */
+                $product->tags = $article->getTags();
+
+                $oManufacturer = $article->getManufacturer();
+                $sManufacturer = $oManufacturer->oxmanufacturers__oxtitle->value;
+                if ($sManufacturer == null) {
+                    $sManufacturer = "";
+                }
+                $product->manufacturer = $sManufacturer;
+
+                $product->category = array();
+                $sCategoryIds = $article->getCategoryIds();
+                if (is_array($sCategoryIds)) {
+                    foreach ($sCategoryIds as $sCategoryId) {
+                        $oCategory = oxNew("oxcategory");
+                        $oCategory->load($sCategoryId);
+
+
+                        $sCategory = $oCategory->oxcategories__oxtitle->value;
+                        $oCategoryParent = $oCategory->getParentCategory();
+                        $sCategoryParent
+                            = $oCategoryParent->oxcategories__oxtitle->value;
+
+                        if ($sCategoryParent == "" || $sCategoryParent == null) {
+                            $sCategoryParent = $sCategory;
+                            $sCategory = "";
+                        }
+
+                        $categoryEntry = array();
+                        $categoryEntry["unterkategorie"] = $sCategory;
+                        $categoryEntry["vaterkategorie"] = $sCategoryParent;
+
+                        $product->category[] = $categoryEntry;
+                    }
+                }
+
+
+
+                $product->inBasketLink = $article->getToBasketLink() . "&amp;am=1";
+                /*
+                Textzusatz ist nötig, da ansonsten kein Artikel
+                in Warenkorb getan wird.
+                */
+
+                $nStock = $article->oxarticles__oxstock->value;
+                $product->stock = $nStock;
+                $fOrgPrice = $article->getFTPrice();
+                if ($fOrgPrice == null) {
+                    $fOrgPrice = $article->getFPrice();
+                }
+                $product->orgPrice = $fOrgPrice;
+
+                /*
+                 * getArticleRatingCount() nicht verwendet,
+                 * da es diese Funktion erst ab Oxid 4.6.0 gibt
+                $sRatingCount = $article->getArticleRatingCount();
+                */
+                $sRatingCount = $article->oxarticles__oxratingcnt->value;
+                $product->ratingCount = $sRatingCount;
+                $sRatingAverage = $article->getArticleRatingAverage();
+                $product->ratingAverage = $sRatingAverage;
+
+                $product->reviews = array();
+                $reviews = $article->getReviews();
+                if (is_array($reviews)) {
+                    foreach ($reviews as $review) {
+                        $nReviewActive = $review->oxreviews__oxactive->value;
+                        if ($nReviewActive == 0) {
+                            $reviewEntry = array();
+                            $reviewEntry['creationdate']
+                                = $review->oxreviews__oxcreate->value;
+                            $reviewEntry['text']
+                                = $review->oxreviews__oxtext->value;
+                            $reviewEntry['firstname']
+                                = $review->oxuser__oxfname->value;
+                            $reviewEntry['rating']
+                                = $review->oxreviews__oxrating->value;
+
+                            $product->reviews[]
+                                = $reviewEntry;
+                        }
+                    }
+                }
+
+                $product->accessoires = array();
+                if (is_array($article->getAccessoires())) {
+                    foreach ($article->getAccessoires() as $accessory) {
+                        $accessoryEntry = array();
+                        $accessoryEntry['title']
+                            = $accessory->oxarticles__oxtitle->rawValue;
+                        $accessoryEntry['price']
+                            = $accessory->getFPrice();
+                        $accessoryEntry['thumb']
+                            = $accessory->getIconUrl();
+                        $accessoryEntry['url_de']
+                            = $accessory->getMainLink();
+                        $accessoryEntry['artnum']
+                            = $accessory->oxarticles__oxartnum->value;
+
+                        $product->accessoires[]
+                            = $accessoryEntry;
+                    }
+                }
+                /* new return values (August 2013) */
+
                 $data[] = $product;
                 $rows->moveNext();
             }
         }
 
-        $dataresult = new GN2_NewsletterConnect_Data_Result;
-        $dataresult->setMeta('rows', $total->fields[0]);
-        $dataresult->setResult($data);
-        return $dataresult;
+        $dataResult = new GN2_NewsletterConnect_Data_Result;
+        $dataResult->setMeta('rows', $total->fields[0]);
+        $dataResult->setResult($data);
+        return $dataResult;
     }
 
 }
